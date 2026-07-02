2a74ecd2139553c1fceb731983fca3b159d99c05
melvoomello-oss <melvoomello@gmail.com>
2026-07-02 10:37:08 +0300

    javascript

---
+++ assests/js/opera.js
@@ -0,0 +1,14 @@
+// javascript code goes here
+   function displayMessage(){
+    alert ("hello, good morning!!")
+
+   }
+   //another function
+    function safari(){
+        alert("you double clicked a button !!")
+    }
+    //another function
+    fuction changecolor(){
+         document.getElementById("mybtn").style.backgroundColor=
+    }
+    
